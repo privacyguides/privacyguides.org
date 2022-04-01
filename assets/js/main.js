8ef8e2a263b4e6fd145eb2c171d407de8591e3b3
Daniel Gray <dng@disroot.org>
2022-04-01 19:29:18 +0000

    Transition to mkdocs (#829)
    Co-authored-by: Jonah Aragon <jonah@triplebit.net>

--- assets/js/main.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll(".onclick-select").forEach(element => {
-  element.addEventListener("click", element.select);
-});
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-})
