a80a1aa9ac49828d469374cdc1fb2dfed8749e62
Jonah Aragon <jonah@triplebit.net>
2022-04-29 10:21:37 -0500

    Update mkdocs-material-insiders (#1147)

--- docs/javascripts/feedback.js
+++ docs/javascripts/feedback.js
@@ -1,5 +1,6 @@
 var feedback = document.forms.feedback
-feedback.removeAttribute("hidden");
+/* Show feedback */
+feedback.hidden = false
 feedback.addEventListener("submit", function(ev) {
   ev.preventDefault()
 
@@ -10,4 +11,13 @@ feedback.addEventListener("submit", function(ev) {
   /* Send feedback value */
   console.log(page, data)
   plausible(data)
+
+  /* Disable form and show note, if given */
+  feedback.firstElementChild.disabled = true
+  var note = feedback.querySelector(
+    ".md-feedback__note [data-md-value='" + data + "']"
+  )
+
+  if (note)
+    note.hidden = false
 })
